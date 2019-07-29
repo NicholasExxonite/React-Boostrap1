3d33b0d6228f535e480190b407d92f9a2e48e84d
Nikolay <nikolaypankov99@gmail.com>
2019-07-29 12:47:44 +0300

    Linked pages, added nav-bar and a welcome header with an image.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
+import {Home} from './Home';
+import {Contact} from './Contact';
+import {About} from './About';
+import NoMatch from  './NoMatch';
+import Layout from './components/Layout';
+import NavigationBar from './components/NavigaitonBar';
+import Jumbotron from './components/Jumbotron';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  render(){
+    return(
+      // Creates a wrapper, so you don't have to se a div
+      //Use switch to be able to switch between different routes
+      // match exactly so it doesn't match with a bunch of things - exact path="/"
+      //if it doesn't match anything will return a 404 error page
+   <React.Fragment>
+      <Router>
+        <NavigationBar />
+        <Jumbotron />
+        <Layout>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/about" component={About} />
+            <Route path="/contact" component={Contact} />
+            <Route component={NoMatch}/>
+          </Switch>
+        </Layout>
+      </Router>
+    </React.Fragment>
+    );
+  }
 }
 
 export default App;
---
+++ src/NoMatch.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+// class NoMatch extends React.Component{
+//     render(){
+//         return(
+//             <div>
+//                 <h2>No Match!</h2>
+//             </div>
+//         );
+//     }
+// }
+export const NoMatch = (props) =>{
+    return(
+    <div>
+    <h2>No Match!</h2>
+    </div>
+    );
+};
+
+export default NoMatch;
